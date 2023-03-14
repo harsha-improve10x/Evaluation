e9f9e7d56590f972756b677e04c72c17acc78d64
Harsha <shreeharsha.a@improve10x.com>
2023-03-14 12:16:09 +0530

    implemeted all examples in SumOfOddNumbers

--- src/SumOfOddNumbers.java
+++ src/SumOfOddNumbers.java
@@ -4,6 +4,18 @@ public class SumOfOddNumbers {
         int[] number = {1, 6, 5, 2, 3};
         int num = findOddNumbers(number);
         System.out.println(num);
+        int[] n = {1, -3, -5, 10, 2};
+        int numbers = findOddNumbers(n);
+        System.out.println(numbers);
+        int[] arr = {2, 4, 6, 8, 10};
+        int array = findOddNumbers(arr);
+        System.out.println(array);
+        int[] a = {1,1,1,2,5};
+        int b = findOddNumbers(a);
+        System.out.println(b);
+        int [] c = {2, 3, 5, 9, 7};
+        int d = findOddNumbers(c);
+        System.out.println(d);
     }
 
     public static int findOddNumbers(int[] num) {
